1365cb00de60b8f99f62cdea461460c8a45d9e87
summermong <2514o_o@naver.com>
2023-09-18 18:47:29 +0900

    feat: 회원가입 폼

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Signup from './Pages/Signup';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Signup />
+    </div>
+  );
 }
 
 export default App;
