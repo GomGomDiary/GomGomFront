bcbadce254e6624393fe6fac55aac88a8b707d86
summermong <2514o_o@naver.com>
2023-09-21 16:15:58 +0900

    feat: 메인 CSS 작업

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,41 +1,10 @@
-import axios from 'axios';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import styles from './Main.module.css';
 
 const Main = () => {
-  const navigate = useNavigate();
-
-  const click = () => {
-    axios
-      .get('https://9c62-221-145-16-224.ngrok-free.app/auth', {
-        withCredentials: true,
-      })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        alert('로그인에 실패했습니다.');
-        console.error(error);
-      });
-  };
-
-  const logout = () => {
-    axios
-      .post('https://9c62-221-145-16-224.ngrok-free.app/logout')
-      .then((response) => {
-        console.log('쿠키 삭제!');
-        navigate('/login', { replace: true });
-      })
-      .catch((error) => {
-        alert('로그아웃에 실패했습니다.');
-        console.error(error);
-      });
-  };
-
   return (
-    <div>
-      <button onClick={click}>로그인 성공!</button>
-      <button onClick={logout}>로그아웃!</button>
+    <div className={styles.main}>
+      <div className={styles.background}>곰곰 다이어리에 온 걸 환영해...</div>
     </div>
   );
 };
