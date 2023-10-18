a1391d99fb81618518d97579030b9c0f7f80e1db
yoyoo <mcwatney@naver.com>
2023-10-18 15:49:01 +0900

    Test-7

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import styles from './Main.module.css';
-import { useCookies } from 'react-cookie';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Main.module.css";
+import { useCookies } from "react-cookie";
 
 const Main = () => {
-  const [cookie, setCookie, removeCookie] = useCookies(['token']);
+  const [cookie, setCookie, removeCookie] = useCookies(["token"]);
   const navigate = useNavigate();
 
   const logout = () => {
-    removeCookie('token');
-    navigate('/login');
+    removeCookie("token");
+    navigate("/login");
   };
 
   return (
     <div className={styles.main}>
       <div className={styles.background}>
-        <button onClick={logout}>로그아웃 버튼</button>
+        <button onClick={logout}>로그아웃</button>
       </div>
     </div>
   );
