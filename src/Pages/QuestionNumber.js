27c8a50daf9e571b289395be5b4063c738a361dd
summermong <2514o_o@naver.com>
2023-10-29 19:35:09 +0900

    feat: 문제 번호 페이지

---
+++ src/Pages/QuestionNumber.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+import { useRecoilState } from 'recoil';
+import { QuestionNum } from '../store/QuestionNum';
+
+import Btn from '../components/Btn';
+import Styles from './QuestionNumber.module.css';
+
+const QuestionNumber = ({ setStep2, setStep3 }) => {
+  const [questionNumber, setQuestionNumber] = useRecoilState(QuestionNum);
+
+  const goToQuestionList = () => {
+    setStep2(false);
+    setStep3(true);
+  };
+
+  const numMinus = () => {
+    if (questionNumber > 3) {
+      setQuestionNumber(questionNumber - 1);
+    }
+  };
+
+  const numPlus = () => {
+    if (questionNumber < 10) {
+      setQuestionNumber(questionNumber + 1);
+    }
+  };
+
+  return (
+    <div className={Styles.QuestionNumber}>
+      <div className={Styles.title}>
+        최소 3개 ~ 최대 10개로<p></p>질문 개수를 정해주세요.
+      </div>
+      <div className={Styles.circle}>
+        <button className={Styles.minus} onClick={numMinus}>
+          -
+        </button>
+        <div className={Styles.number}>{questionNumber}</div>
+        <button className={Styles.plus} onClick={numPlus}>
+          +
+        </button>
+      </div>
+      <Btn text={'다음'} onClick={goToQuestionList} />
+    </div>
+  );
+};
+
+export default QuestionNumber;
