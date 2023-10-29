0ebfaf645fce3518642717144fbf1de5c41d5712
summermong <2514o_o@naver.com>
2023-10-30 00:51:06 +0900

    feat: Recoil 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
 import React from 'react';
+import { BrowserRouter, Route, Routs } from 'react-router-dom';
+
+import Header from './Home/Header';
+import Main from './Home/Main';
+import { RecoilRoot } from 'recoil';
 
 function App() {
-  return <div className="App">안녕</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <RecoilRoot>
+        <div className="App">
+          <Main />
+        </div>
+      </RecoilRoot>
+    </BrowserRouter>
+  );
 }
 
 export default App;
