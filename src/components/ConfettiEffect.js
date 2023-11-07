4157501ef4386e5b577a999cfa1f971887d1079c
summermong <2514o_o@naver.com>
2023-11-07 10:50:27 +0900

    FIX: 컨페티 효과 수정

--- src/components/ConfettiEffect.js
+++ src/components/ConfettiEffect.js
@@ -5,7 +5,7 @@ const ConfettiEffect = () => {
   useEffect(() => {
     canvasConfetti({
       particleCount: 150,
-      spread: 60,
+      spread: 50,
       origin: { x: 0.5, y: 0.7 },
     });
   }, []);
