5127d3228410c45b331e3ef804d8602a915da2bf
summermong <2514o_o@naver.com>
2023-11-26 21:31:54 +0900

    FEAT: axios 모듈 작업

---
+++ src/api/customAxios.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const instance = (answererJWT) => {
+  const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_SERVER_URL,
+    withCredentials: true,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${answererJWT}`,
+    },
+  });
+
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => Promise.reject(error)
+  );
+  return axiosInstance;
+};
+
+export default instance;
