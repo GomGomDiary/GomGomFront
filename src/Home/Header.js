1a7b15665c686696a937289fe3a2818b8083bd4f
summermong <2514o_o@naver.com>
2023-12-03 18:20:24 +0900

    FEAT: 히스토리 기능 추가 작업중

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Styles from './Header.module.css';
+import { MdOutlineHistory } from 'react-icons/md';
 
 const Header = () => {
-  return <div className={Styles.Header}>GomGom Diary 🐻💭 </div>;
+  return (
+    <div className={Styles.Header}>
+      <MdOutlineHistory className={Styles.float} />
+      <div className={Styles.title}>GomGom Diary 🐻💭</div>
+    </div>
+  );
 };
 
 export default Header;
