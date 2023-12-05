a3b840d1b4e7a21b73ec004581df54642e626519
summermong <2514o_o@naver.com>
2023-12-05 20:20:10 +0900

    FEAT: 다이어리 히스토리 개발중 얼럿 띄우기

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -3,9 +3,13 @@ import Styles from './Header.module.css';
 import { MdOutlineHistory } from 'react-icons/md';
 
 const Header = () => {
+  const handleAlert = () => {
+    alert('개발중입니다! 조금만 기다려주세요.');
+  };
+
   return (
     <div className={Styles.Header}>
-      <MdOutlineHistory className={Styles.float} />
+      <MdOutlineHistory className={Styles.float} onClick={handleAlert} />
       <div className={Styles.title}>GomGom Diary 🐻💭</div>
     </div>
   );
