86b6ac8fd79877df6cedbf3986bc2e9e0d088109
summermong <2514o_o@naver.com>
2023-12-07 03:12:26 +0900

    FEAT: CustomModal 작업 & CSS
    Related to: #28

--- src/components/CustomModal.js
+++ src/components/CustomModal.js
@@ -1,51 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 import Styles from './CustomModal.module.css';
 
-const CustomModal = () => {
-  const [modalIsOpen, setModalIsOpenState] = useState(true);
+const CustomModal = ({ message, updateModal }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(true);
 
   const handleClose = () => {
-    setModalIsOpenState(false);
+    setModalIsOpen(false);
+    updateModal(false);
   };
 
   return (
     <Modal
       className={Styles.Modal}
+      overlayClassName={Styles.ModalOverlay}
       isOpen={modalIsOpen}
       appElement={document.getElementById('root')}
-      style={{
-        overlay: {
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          backgroundColor: 'rgba(255, 255, 255, 0.7)',
-          zIndex: '1',
-        },
-        content: {
-          position: 'absolute',
-          height: '100px',
-          width: '250px',
-          top: '60px',
-          left: '50%',
-          transform: 'translateX(-50%)',
-          bottom: '40px',
-          border: '1px solid #ccc',
-          background: '#fff',
-          overflow: 'auto',
-          WebkitOverflowScrolling: 'touch',
-          borderRadius: '4px',
-          outline: 'none',
-          padding: '20px',
-        },
-      }}
+      onRequestClose={handleClose}
     >
-      <button className={Styles.cancelBtn} onClick={handleClose}>
-        ❌
-      </button>
-      <div>안녕</div>
+      <div className={Styles.ModalContent}>
+        <div>{message}</div>
+        <button className={Styles.okBtn} onClick={handleClose}>
+          확인
+        </button>
+      </div>
     </Modal>
   );
 };
