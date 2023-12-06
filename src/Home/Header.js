518d0432ff86755d55b769830f30ce2dd00d193b
summermong <2514o_o@naver.com>
2023-12-07 03:29:15 +0900

    FEAT: 히스토리 버튼 클릭 시 모달 적용
    Related to: #28

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styles from './Header.module.css';
 import { MdOutlineHistory } from 'react-icons/md';
+import CustomModal from '../components/CustomModal';
 
 const Header = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
   const handleAlert = () => {
-    alert('개발중입니다! 조금만 기다려주세요.');
+    setIsClicked(true);
+  };
+
+  const handleModalClose = () => {
+    setIsClicked(false);
   };
 
   return (
     <div className={Styles.Header}>
-      <MdOutlineHistory className={Styles.float} onClick={handleAlert} />
+      <MdOutlineHistory className={Styles.history} onClick={handleAlert} />
       <div className={Styles.title}>GomGom Diary 🐻💭</div>
+      {isClicked && (
+        <CustomModal
+          message={`현재 개발중입니다. 조금만 기다려주세요 :)`}
+          updateModal={handleModalClose}
+        />
+      )}
     </div>
   );
 };
