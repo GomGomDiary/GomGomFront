8dab51c7024ab0c067f45682525ae796ae11d439
summermong <2514o_o@naver.com>
2023-12-07 03:34:41 +0900

    FEAT: 챌린지 미설정 시 모달 적용
    Related to: #28

--- src/Pages/Create/WriteChallenge.js
+++ src/Pages/Create/WriteChallenge.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Styles from './WriteChallenge.module.css';
 import { useRecoilState } from 'recoil';
 import { Challenge } from '../../store/Create/Challenge';
@@ -6,9 +6,12 @@ import { Challenge } from '../../store/Create/Challenge';
 import Input from '../../components/Input';
 import Btn from '../../components/Btn';
 import WhiteBtn from '../../components/WhiteBtn';
+import CustomModal from '../../components/CustomModal';
 
 const WriteChallenge = ({ onNextStep, onPreviousStep }) => {
   const [challenge, setChallenge] = useRecoilState(Challenge);
+  const [isSetted, setIsSetted] = useState(false);
+
   const challengeInputRef = useRef();
 
   const writingChallenge = (e) => {
@@ -25,18 +28,28 @@ const WriteChallenge = ({ onNextStep, onPreviousStep }) => {
     if (challenge) {
       onNextStep();
     } else {
-      alert('암호를 입력해주세요.');
+      setIsSetted(true);
       challengeInputRef.current.focus();
     }
   };
 
+  const handleModalClose = () => {
+    setIsSetted(false);
+  };
+
   return (
     <div className={Styles.WriteChallengeContainer}>
+      {isSetted && (
+        <CustomModal
+          message={'암호를 설정해주세요.'}
+          updateModal={handleModalClose}
+        />
+      )}
       <div className={Styles.top}>
         <div>🔒</div>
-        <div>모든 질문이 완성됐어요!</div>
-        <div>특별한 사람만 답할 수 있게 암호를 설정하세요.</div>
-        <div>암호는 신중하고 정확하게 입력해주세요.</div>
+        <div>모든 질문이 완성됐다곰!</div>
+        <div>우리만의 암호를 아는 사람만 답장할 수 있도록</div>
+        <div>암호는 정확하게 입력해주세요.</div>
       </div>
       <div className={Styles.middle}>
         <Input
@@ -44,7 +57,7 @@ const WriteChallenge = ({ onNextStep, onPreviousStep }) => {
           value={challenge}
           onChange={(e) => writingChallenge(e)}
           onKeyUp={handleKeyPress}
-          placeholder="암호를 입력하세요."
+          placeholder="ex. 내 생일 4자리"
           ref={challengeInputRef}
         />
       </div>
