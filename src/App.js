8fd97bee518c1d2c41387290e08595c99a30157d
summermong <2514o_o@naver.com>
2023-12-09 23:31:11 +0900

    FEAT: 히스토리 관련 route 추가
    Resolves: #27

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Main from './Home/Main';
 import { RecoilRoot } from 'recoil';
 import { CookiesProvider } from 'react-cookie';
 import NotFound from './Pages/NotFound';
+import History from './Pages/History/History';
+import HistoryItem from './Pages/History/HistoryItem';
 
 function App() {
   return (
@@ -15,9 +17,12 @@ function App() {
         <BrowserRouter>
           <Header />
           <Routes>
+            <Route path="/history" element={<History />} />
+            <Route path="/history/:historyItemId" element={<HistoryItem />} />
             <Route path="/answerers/:diaryId" element={<Main />} />
             <Route path="/answer/:diaryId/:answerId" element={<Main />} />
             <Route path="/diary/:diaryId" element={<Main />} />
+
             <Route path="/" element={<Main />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
