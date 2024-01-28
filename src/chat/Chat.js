a2a2a40e54a679957d812a720e4c25672cbbeae0
summermong <2514o_o@naver.com>
2024-01-29 02:44:54 +0900

    FEAT: 채팅 수신/발신 내역 작업중

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -1,28 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Styles from './Chat.module.css';
 import Header from '../Home/Header';
-import SendMessage from './SendMessage';
-import ReceiveMessage from './ReceiveMessage';
+import SendMessage from '../chat/SendMessage';
+import ReceiveMessage from '../chat/ReceiveMessage';
 import { io } from 'socket.io-client';
 import { useRecoilValue } from 'recoil';
 import { ChatToken } from '../store/Chat/ChatToken';
 import { RoomId } from '../store/Chat/RoomId';
 import instance from '../api/customAxios';
 import { useNavigate } from 'react-router-dom';
+import { GuestAddress } from '../store/Chat/GuestAddress';
+import { getCookie } from '../api/cookie';
 
 const Chat = () => {
   const chatToken = useRecoilValue(ChatToken);
   const roomId = useRecoilValue(RoomId);
 
-  const navigate = useNavigate('');
+  const navigate = useNavigate();
 
   const [socket, setSocket] = useState(null);
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const [receivedMessages, setReceivedMessages] = useState([]);
-
   const [questioner, setQuestioner] = useState('');
   const [answerer, setAnswerer] = useState('');
+
+  const guestAddress = useRecoilValue(GuestAddress);
+  const diaryAddress = getCookie('diaryAddress');
 
   const getNicknames = async () => {
     try {
@@ -41,6 +44,8 @@ const Chat = () => {
       roomid: `${roomId}`,
     };
 
+    console.log(header);
+
     const newSocket = io.connect(`${process.env.REACT_APP_CHAT_WS}`, {
       extraHeaders: {
         ...header,
@@ -54,42 +59,60 @@ const Chat = () => {
 
     newSocket.emit('enter_room', { roomId });
 
-    newSocket.emit('exception', (e) => {
+    newSocket.on('exception', (e) => {
       console.error('에러남: ', e);
     });
 
     newSocket.on('disconnect', () => {
-      console.log('enlfhrkdy');
+      console.log('뒤로 돌아가기');
       navigate(-1);
     });
 
     getNicknames();
-
-    newSocket.on('receive_message', (message) => {
-      console.log('수신된 메세지:', 'ㅇㅇ');
-      const newReceivedMessage = {
-        chat: message.text,
-        nickname: message.sender,
-        time: new Date(),
-      };
-
-      setReceivedMessages((prevReceivedMessages) => [
-        ...prevReceivedMessages,
-        newReceivedMessage,
-      ]);
-    });
 
     return () => {
       if (socket) {
         socket.disconnect();
       }
     };
-  }, []);
+  }, [roomId]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('receive_message', (message) => {
+        const newMessage = {
+          chat: message.chat,
+          nickname: message.nickname,
+          time: new Date(),
+        };
+
+        setMessages((prevReceivedMessages) => [
+          ...prevReceivedMessages,
+          newMessage,
+        ]);
+      });
+    }
+  }, [socket]);
+
+  const scrollContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollTop =
+        scrollContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
 
   const handleMessageSend = (e) => {
     if (socket && message.trim() !== '' && e.key === 'Enter') {
-      const nickname = 'answerer';
-      const newMessage = { sender: nickname, text: message, time: new Date() };
+      const nickname = guestAddress === diaryAddress ? answerer : questioner;
+
+      const newMessage = {
+        nickname: nickname,
+        chat: message,
+        time: new Date(),
+        isSender: true,
+      };
 
       socket.emit('send_message', {
         roomId: roomId,
@@ -109,9 +132,16 @@ const Chat = () => {
   return (
     <div className={Styles.Chat}>
       <Header />
-      <div className={Styles.ChatContainer}>
-        <SendMessage messages={messages} />
-        <ReceiveMessage receivedMessages={receivedMessages} />
+      <div className={Styles.ChatContainer} ref={scrollContainerRef}>
+        {messages.map((message, i) => (
+          <div key={i}>
+            {message.isSender ? (
+              <SendMessage message={message} />
+            ) : (
+              <ReceiveMessage message={message} />
+            )}
+          </div>
+        ))}
       </div>
       <input
         className={Styles.messageInput}
