6a44522859c41d9e976993ccc2401f511fcf2a82
summermong <2514o_o@naver.com>
2024-01-31 01:42:05 +0900

    FEAT: 수신/발신 메세지 시간 표시 작업

--- src/chat/ReceiveMessage.js
+++ src/chat/ReceiveMessage.js
@@ -2,13 +2,18 @@ import React from 'react';
 import Styles from './ReceiveMessage.module.css';
 
 const ReceiveMessage = ({ message }) => {
+  const time = new Date(message.createdAt).toLocaleString('ko-kr', {
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
   return (
     <div className={Styles.ReceiveMessage}>
       <div className={Styles.receiver}>🐻 {message.nickname}</div>
       <div className={Styles.rMessage}>{message.chat}</div>
-      <div className={Styles.rTime}>
-        {message.time.toLocaleTimeString().slice(0, -3)}
-      </div>
+      <div className={Styles.rTime}>{time}</div>
     </div>
   );
 };
--- src/chat/SendMessage.js
+++ src/chat/SendMessage.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Styles from './SendMessage.module.css';
 
 const SendMessage = ({ message }) => {
+  const time = new Date(message.createdAt).toLocaleString('ko-kr', {
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
   return (
     <div className={Styles.SendMessage}>
       <div className={Styles.sender}>🍯 {message.nickname}</div>
       <div className={Styles.sMessage}>{message.chat}</div>
 
-      <div className={Styles.sTime}>
-        {message.time.toLocaleTimeString().slice(0, -3)}
-      </div>
+      <div className={Styles.sTime}>{time}</div>
     </div>
   );
 };
