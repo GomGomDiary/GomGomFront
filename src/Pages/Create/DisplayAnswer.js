dde9ded525439b6e9af5618ca2819b4e661eebe8
summermong <2514o_o@naver.com>
2024-02-10 17:49:20 +0900

    FIX: 답변 조회 시 recoil 관련 버그 수정
    - recoil에 저장된 값을 가져오기에 새로고침처럼 휘발될 경우 length를 읽지 못합니다.
    - 옵셔널 체이닝으로 값의 존재 여부를 확인하고 없으면 뒤로 가기, 있으면 읽어옵니다.

--- src/Pages/Create/DisplayAnswer.js
+++ src/Pages/Create/DisplayAnswer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Styles from './DisplayAnswer.module.css';
 import { useNavigate } from 'react-router-dom';
 import Btn from '../../components/Btn';
@@ -15,16 +15,22 @@ const DisplayAnswer = () => {
 
   const arr = [];
 
-  for (let i = 0; i < answer.answers.length; i++) {
+  useEffect(() => {
+    if (!answer?.answers || answer.answers.length === 0) {
+      navigate(-1);
+    }
+  }, [answer, navigate]);
+
+  for (let i = 0; i < answer?.answers?.length; i++) {
     arr.push(`${i + 1}번 `);
-    arr.push(`Q: ${question.question[i]}`);
-    arr.push(`A: ${answer.answers[i]}`);
+    arr.push(`Q: ${question?.question?.[i]}`);
+    arr.push(`A: ${answer?.answers?.[i]}`);
   }
 
   return (
     <div className={Styles.DisplayAnswerContainer}>
       <div className={Styles.DisplayAnswer}>
-        <div className={Styles.answerer}>{answer.answerer}님의 답장💌</div>
+        <div className={Styles.answerer}>{answer?.answerer}님의 답장💌</div>
         <div className={Styles.answer}>
           {arr.map((num, idx) => (
             <div key={idx}>{num}</div>
