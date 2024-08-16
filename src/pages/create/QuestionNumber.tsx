d7f7f4a25bf909b492565814d9e83a73f6f74414
summermong <2514o_o@naver.com>
2024-08-16 22:59:24 +0900

    feat: QuestionNumber 페이지 작업

--- src/pages/create/QuestionNumber.tsx
+++ src/pages/create/QuestionNumber.tsx
@@ -1,4 +1,57 @@
+import { questionerAtom } from '@/store/create/questioner';
+import styles from './QuestionNumber.module.css';
+import Button from '@/components/Button';
+import { questionNumberAtom } from '@/store/create/questionNumber';
+import { useAtom, useAtomValue } from 'jotai';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const QuestionNumber = () => {
-  return <div>QuestionNumber</div>;
+  const navigate = useNavigate();
+  const questioner = useAtomValue(questionerAtom);
+
+  useEffect(() => {
+    if (questioner.length === 0) {
+      navigate('/');
+    }
+  }, [navigate, questioner.length]);
+
+  const [questionNum, setQuestionNum] = useAtom(questionNumberAtom);
+
+  const handleNumMinus = () => {
+    if (questionNum > 3) {
+      setQuestionNum(questionNum - 1);
+    }
+  };
+
+  const handleNumPlus = () => {
+    if (questionNum < 10) {
+      setQuestionNum(questionNum + 1);
+    }
+  };
+
+  const goToQuestionList = () => {
+    navigate('/question-list');
+  };
+
+  return (
+    <div className={styles.QuestionNumber}>
+      <div className={styles.title}>
+        <p>최소 3개부터 최대 10개로</p>
+        <p>질문 개수를 정해달라곰!</p>
+      </div>
+      <div className={styles.circle}>
+        <button className={styles.minus} onClick={handleNumMinus}>
+          -
+        </button>
+        <div className={styles.number}>{questionNum}</div>
+        <button className={styles.plus} onClick={handleNumPlus}>
+          +
+        </button>
+      </div>
+      <Button text={'다음'} variant="default" onClick={goToQuestionList} />
+    </div>
+  );
 };
+
 export default QuestionNumber;
