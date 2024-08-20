21a7a4934536f10588614664e63dcae2873cf0c3
summermong <2514o_o@naver.com>
2024-08-20 15:33:50 +0900

    feat: atom 설정

--- src/store/Chat/ChatToken.js
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const ChatToken = atom({
-  key: 'ChatToken',
-  default: '',
-});
--- src/store/Response/AnswererCookie.js
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const AnswererCookie = atom({
-  key: 'AnswererCookie',
-  default: '',
-});
