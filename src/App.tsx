1a687e65ad90970ce76fe76996dc77ba216411b8
summermong <2514o_o@naver.com>
2024-10-16 22:05:26 +0900

    FIX: App 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,23 @@ import QuestionNumber from './pages/create/QuestionNumber';
 import QuestionList from './pages/create/QuestionList';
 import WriteChallenge from './pages/create/WriteChallenge';
 import WriteCountersign from './pages/create/WriteCountersign';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+import Finish from './pages/create/Finish';
+import History from './pages/history/History';
+import HistoryItem from './pages/history/HistoryItem';
+import MatchChallenge from './pages/response/MatchChallenge';
+import DisplayAnswerList from './pages/create/DisplayAnswerList';
+import DisplayAnswer from './pages/create/DisplayAnswer';
+import NotFound from './pages/NotFound';
 
 const App = () => {
+  useEffect(() => {
+    if (!window.location.href.includes('localhost')) {
+      ReactGA.initialize(`${process.env.REACT_APP_GA}`);
+    }
+  }, []);
+
   return (
     <CookiesProvider>
       <BrowserRouter>
@@ -18,6 +33,15 @@ const App = () => {
             <Route path="/question-list" element={<QuestionList />} />
             <Route path="/challenge" element={<WriteChallenge />} />
             <Route path="/countersign" element={<WriteCountersign />} />
+            <Route path="/finish" element={<Finish />} />
+            <Route path="/answerers/:diaryId" element={<DisplayAnswerList />} />
+            <Route path="/answer/:diaryId" element={<DisplayAnswer />} />
+
+            <Route path="/diary/:diaryId" element={<MatchChallenge />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/history/:historyItemId" element={<HistoryItem />} />
+
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
