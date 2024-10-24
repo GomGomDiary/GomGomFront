d87413b27db11f853dbea729bf69809e25c8b99e
summermong <2514o_o@naver.com>
2024-10-24 18:26:30 +0900

    CHORE: store 파일/변수명 변경

---
+++ src/store/answer/answererToken.ts
@@ -0,0 +1,3 @@
+import { atom } from 'jotai';
+
+export const answererTokenAtom = atom('');
---
+++ src/store/answer/index.ts
@@ -0,0 +1,3 @@
+export * from './answerArr';
+export * from './answerer';
+export * from './answererToken';
